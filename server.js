cff723afc95230d213db8efeb038cfb351bf5729
BrantBart <brant.barton64@gmail.com>
2024-02-03 16:27:45 -0700

    validation testing

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const mongodb = require("./data/database");
 const bodyParser = require("body-parser");
+const { signupValidation, loginValidation } = require("./validation.js");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-// can work on different sites?
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -23,6 +24,23 @@ app.use((req, res, next) => {
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", require("./routes"));
+
+app.post("/register", signupValidation, (req, res, next) => {
+  // your registration code
+});
+
+app.post("/login", loginValidation, (req, res, next) => {
+  // your login code
+});
+
+// Handling Errors
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal Server Error";
+  res.status(err.statusCode).json({
+    message: err.message,
+  });
+});
 
 mongodb.initDb((err) => {
   if (err) {
